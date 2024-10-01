83e44a4f884ac302c3ee2bc80cc64c12d2d79770
Natalia <teixeiranatalia935@gmail.com>
2024-10-01 13:22:11 +0000

    Criação da Operação de Adição

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,24 @@
-import "./styles.css";
+//REFERENCIAR ELEMENTOS COM JS
+const app = document.querySelector("#app");
+const numero1 = document.querySelector("#numero1");
+const numero2 = document.querySelector("#numero2");
+const btnCalcular = document.querySelector("#btn-calcular");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+const resultados = document.querySelector(".resultados");
+const soma = document.querySelector(".soma");
+const subtracao = document.querySelector(".subtracao");
+const multiplicacao = document.querySelector(".multiplicacao");
+const divisao = document.querySelector(".divisao");
+
+//CRIAR FUNÇÃO = refatorar ou melhorar
+function calcular() {
+  const n1 = Number(numero1.value);
+  const n2 = Number(numero2.value);
+  soma.innerHTML = "A soma de ${n1} e ${n2} é: ${n1 + n2}";
+}
+
+//ADICIONAR E ESCUTADOR DE EVENTOS
+btnCalcular.addEventListener("click", function (evento) {
+  evento.preventDefault();
+  calcular();
+});
